b88884865a0fc607efd82f64a309d9aad81d20ad
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-03 17:08:38 +0000

    automatically backed up by learn

--- lib/music_library_Controller.rb
+++ lib/music_library_Controller.rb
@@ -44,7 +44,6 @@ class MusicLibraryController
   def list_songs
     Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
       puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
-          input = gets.strip
     end
   end
 
